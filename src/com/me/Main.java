8f4dfb91173d2906512aafd7758671858a022a19
patrickatcw <pj8392@yahoo.com>
2018-03-14 11:06:38 -0400

    updated gitignore file and created initial program structure, directions

---
+++ src/com/me/Main.java
@@ -0,0 +1,25 @@
+package com.me;
+
+//directions
+/*
+- create a simple banking application
+- need a bank class
+- shoulsd have an arraylist of branches
+- each branch should have an arraylist of customers
+- the customer class should have an arraylist of doubles (transactions)
+- customer; name, and the arraylist of doubles
+- branch; need to be able to add a new customer nd initial transaction amount
+- also needs to add additional transactions for that customer / branch
+- bank; add a new branch, add a customer to that branch with initial transaction
+- add a transaction for an existing customer for that branch
+- show a list of customers for a particular branch and optionally a list
+of their transactions
+- demonstrate autoboxing, and unboxing
+hint; transactions
+- add data validation
+- ex. check if exists, or does not exist, etc.
+- think about where you are adding the code to perform certain actions
+ */
+
+public class Main {
+}
